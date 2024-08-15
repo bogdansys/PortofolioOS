295f8a4bb5fd1e177f9e66385fc4e9c4422e5ba7
Mihai Bogdan Iordache <darksec@dark.com>
2024-08-15 14:46:41 +0200

    default light theme

--- src/components/ThemeProvider.jsx
+++ src/components/ThemeProvider.jsx
@@ -16,17 +16,8 @@ export function ThemeProvider({
 
     root.classList.remove("light", "dark")
 
-    if (theme === "system") {
-      const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
-        .matches
-        ? "dark"
-        : "light"
-
-      root.classList.add(systemTheme)
-      return
-    }
-
-    root.classList.add(theme)
+    // Always set the theme to light
+    root.classList.add("light")
   }, [theme])
 
   const value = {
